2768101e1ac156ee4340ae4288c6486c8e659efa
jakub39 <kubo.pos@gmail.com>
2023-05-02 21:51:21 +0200

    zadanie

---
+++ scroll.js
@@ -0,0 +1,9 @@
+let header = document.getElementById("t");
+header.style.opacity = 0.75; //priehladnost navbaru 25%
+
+//zvysenie priehladnosti na 0% pri scrolle
+window.addEventListener("scroll", function () {
+    let header = document.getElementById("t");
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    header.style.opacity = 0.75 + scrollTop  / 1000; 
+});

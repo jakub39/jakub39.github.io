ab94a269917fb327160ef43d9a3784040aac2605
jakub39 <kubo.pos@gmail.com>
2023-05-15 22:59:34 +0200

    zadanie2

--- dzavaskript.js
+++ dzavaskript.js
@@ -1,10 +1,19 @@
 let header = document.getElementById("t");
-header.style.opacity = 0.75; //priehladnost navbaru 25%
+header.style.opacity = 0.65; //priehladnost navbaru 25%
 
 //znizenie priehladnosti na 0% pri scrolle
 window.addEventListener("scroll", function () {
     let header = document.getElementById("t");
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    header.style.opacity = 0.75 + scrollTop  / 1100; 
+    header.style.opacity = 0.65 + scrollTop  / 800; 
 });
 
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    document.getElementById("logo").style.height = "100px";
+    } else {
+    document.getElementById("logo").style.height = "120px";
+    }
+}

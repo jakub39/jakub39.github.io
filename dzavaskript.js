f3591b34d1ab4599c91903eff4457d9a3b97578c
jakub39 <kubo.pos@gmail.com>
2023-05-26 23:42:20 +0200

    zadanie2

--- dzavaskript.js
+++ dzavaskript.js
@@ -1,25 +1,42 @@
 let logo = document.getElementById("logo");
-logo.style.height = "120px";
+logo.style.height = "110px";
 
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    logo.style.height = "100px";
+    logo.style.height = "80px";
     } 
     else {
-    logo.style.height = "120px";
+    logo.style.height = "110px";
     }
 }
 
 let header = document.getElementById("t");
-header.style.opacity = 0.65; //priehladnost navbaru 25%
+header.style.opacity = 0.75; //priehladnost navbaru 75%
 
 //znizenie priehladnosti na 0% pri scrolle
 window.addEventListener("scroll", function () {
     let header = document.getElementById("t");
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    header.style.opacity = 0.65 + scrollTop / 800;
+    header.style.opacity = 0.75 + scrollTop / 900;
 });
 
 
-//Galeria
+//Zobraznenie elementov
+function reveal() {
+    var reveals = document.querySelectorAll(".zobrazenie");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 90;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+window.addEventListener("scroll", reveal);

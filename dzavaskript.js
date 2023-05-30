1f21ec2691b1604b0ea146bf3e1bdb5842ea290d
jakub39 <kubo.pos@gmail.com>
2023-05-30 20:08:55 +0200

    zadanie2

--- dzavaskript.js
+++ dzavaskript.js
@@ -3,32 +3,68 @@ logo.style.height = "110px";
 
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
-    if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-    logo.style.height = "80px";
+    if (document.body.scrollTop > 130 || document.documentElement.scrollTop > 130) {
+    logo.style.height = "75px";
     } 
     else {
-    logo.style.height = "110px";
+    logo.style.height = "100px";
     }
 }
 
-//Zobraznenie elementov animacia zdola
+//Zobraznenie elementov animacie
+//Zdola hore
 function reveal() {
-    var reveals = document.querySelectorAll(".zobrazenie");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 90;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
-      } else {
-        reveals[i].classList.remove("active");
-      }
+  var reveals = document.querySelectorAll(".zobrazenie");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 90;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
     }
   }
+}
 window.addEventListener("scroll", reveal);
 
+//Zlava do prava
+function reveal2() {
+  var reveals = document.querySelectorAll(".zobrazenie2");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var triggerPoint = windowHeight * 0.75; // Adjust the trigger point as needed
+
+    if (elementTop < triggerPoint) {
+      reveals[i].classList.add("active2");
+    } else {
+      reveals[i].classList.remove("active2");
+    }
+  }
+}
+window.addEventListener("scroll", reveal2);
+
+//Zprava do lava
+function reveal3() {
+  var reveals = document.querySelectorAll(".zobrazenie3");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var triggerPoint = windowHeight * 0.75; // Adjust the trigger point as needed
+
+    if (elementTop < triggerPoint) {
+      reveals[i].classList.add("active3");
+    } else {
+      reveals[i].classList.remove("active3");
+    }
+  }
+}
+window.addEventListener("scroll", reveal3);
 
 //Navbar na hlavnej stranke
 let header = document.getElementById("t");
